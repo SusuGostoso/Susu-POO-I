fc15c0a7183b6d0e9b40a01a883af846a35bf493
SusuGostoso <suelitonmdr@gmail.com>
2023-04-04 12:33:32 -0300

    Exercício 2/11 finalizado: estilizar Textos do body

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,39 @@ void main() {
         appBar: AppBar(title: Text("Meu app")),
         body: Center(
             child: Column(children: [
-          Text("Apenas começando..."),
-          Text("No meio..."),
-          Text("Terminando...")
+          Text(
+            "Apenas começando...",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "Roboto",
+              letterSpacing: 1.5,
+              color: Colors.deepOrange,
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+          Text(
+            "Isso parece um link bem no seu meio",
+            style: TextStyle(
+              color: Colors.blue,
+              backgroundColor: Colors.white,
+              fontStyle: FontStyle.italic,
+              wordSpacing: 5.0,
+              height: 1.5,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.blue,
+              decorationThickness: 2.0,
+            ),
+          ),
+          Text(
+            "THE END",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "Georgia",
+              letterSpacing: 3.5,
+              color: Colors.indigo,
+              decoration: TextDecoration.none,
+            ),
+          )
         ])),
         bottomNavigationBar: Text("Botão 1"),
       ));
