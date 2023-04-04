fd4c4e22a18e3e3a23806e86b5b91d0e8bb9a265
SusuGostoso <suelitonmdr@gmail.com>
2023-04-04 12:36:54 -0300

    Exercício 3/11 finalizado: 3 Texts adicionados ao bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,14 @@ void main() {
             ),
           )
         ])),
-        bottomNavigationBar: Text("Botão 1"),
+        bottomNavigationBar: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Botão 1"),
+            Text("Botão 2"),
+            Text("Botão 3"),
+          ],
+        ),
       ));
 
   runApp(app);
