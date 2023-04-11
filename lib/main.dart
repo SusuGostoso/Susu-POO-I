a7cb6308acc9d8f9d2c35e018831f6d636ce69e8
SusuGostoso <suelitonmdr@gmail.com>
2023-04-11 12:44:25 -0300

    Tarefa 3 [Receita 2] - Exer. 1 fin.: Criar uma caixa para o body do Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,73 @@ class NewNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
-      BottomNavigationBarItem(
-        label: "Cafés",
-        icon: Icon(Icons.coffee_outlined),
-      ),
-      BottomNavigationBarItem(
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-    ]);
+    return BottomNavigationBar(
+      onTap: botaoFoiTocado,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas",
+          icon: Icon(Icons.local_drink_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Nações",
+          icon: Icon(Icons.flag_outlined),
+        )
+      ],
+    );
   }
+}
+
+class NewBody extends StatelessWidget {
+  const NewBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Text("La Fin Du Monde - Bock - 65 ibu"),
+        ),
+        Expanded(
+          child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
+        ),
+        Expanded(
+          child: Text("Duvel - Pilsner - 82 ibu"),
+        ),
+      ],
+    );
+  }
+}
+
+class NewAppBar extends AppBar {
+  NewAppBar()
+      : super(
+          title: Text("Dicas"),
+          backgroundColor: Colors.deepPurple,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => print("Pesquisando..."),
+            ),
+            IconButton(
+              icon: Icon(Icons.favorite_border),
+              onPressed: () => print("Favoritos..."),
+            ),
+          ],
+        );
 }
 
 void main() {
   MaterialApp app = MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: Scaffold(
-        appBar: AppBar(title: Text("Dicas")),
-        body: Column(children: [
-          Expanded(
-            child: Text("La Fin Du Monde - Bock - 65 ibu"),
-          ),
-          Expanded(
-            child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
-          ),
-          Expanded(
-            child: Text("Duvel - Pilsner - 82 ibu"),
-          )
-        ]),
-        bottomNavigationBar: NewNavBar(),
-      ));
+    home: Scaffold(
+      appBar: NewAppBar(),
+      body: NewBody(),
+      bottomNavigationBar: NewNavBar(),
+    ),
+  );
 
   runApp(app);
 }
